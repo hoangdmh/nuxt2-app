a96bffaf50227a79c14a45e4956168ad50b747e9
hoangdmh <dmhoang08t1@gmail.com>
2023-10-30 14:26:01 +0700

    Nested Routes

---
+++ pages/users.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <NuxtLink to="/users">User</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/users/20">User id</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/users/setting">User setting</NuxtLink>
+            </li>
+        </ul>
+        <h1>User page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, recusandae ab dolorem ducimus expedita enim optio consequuntur natus dolor ex vitae, quia quibusdam fugiat, mollitia soluta nisi possimus minus unde.</p>
+
+        <NuxtChild />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/_userId.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>User id page</h1>
+</template>
+
+<script>
+export default {
+    validate(context) {
+        // console.log('context =>', Object.keys(context));
+        // console.log('context =>', Number(context.params.userId));
+        if(!Number(context.params.userId)){
+            return false
+        }else {
+            return true
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
