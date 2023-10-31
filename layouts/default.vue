85a89d6e0587112065599c62fbbedb2f31448719
hoangdmh <dmhoang08t1@gmail.com>
2023-10-31 10:38:21 +0700

    Scroll Behavior NuxtJs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="root">
     <header>Header page</header>
+    <NuxtLink to="/scroll/long-page">Long page</NuxtLink>
     <Nuxt />
     <notifications group="foo" />
   </div>
 </template>
+
+<script>
+  export default {
+    scrollToTop: true
+  }
+</script>
 
 <style>
 
---
+++ pages/scroll/long-page.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="long-page">
+        <h1>Long page</h1>
+        <div class="box">
+            <h2>Box title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <img src="https://placehold.jp/750x500.png" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+        </div>
+        <div class="box">
+            <h2>Box title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <img src="https://placehold.jp/750x500.png" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+        </div>
+        <div class="box">
+            <h2>Box title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <img src="https://placehold.jp/750x500.png" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+        </div>
+        <NuxtLink to="/scroll/another-long-page">AnotherLong page</NuxtLink>
+        <div class="box">
+            <h2>Box title</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <img src="https://placehold.jp/750x500.png" alt="">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi deleniti porro ratione est magnam totam repudiandae facere, velit laborum? Atque dolor ut officia hic voluptates labore odio laboriosam rerum. Eveniet!</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .long-page {
+        width: 960px;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .box {
+        background-color: rgb(41, 140, 206);
+        padding: 20px;
+        margin-bottom: 50px;
+    }
+</style>
